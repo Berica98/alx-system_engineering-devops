e4ecb0ad83106f6f3a971b528158ce8dd23368f3
Berica98 <berica.george@gmail.com>
2024-09-27 05:54:01 -0700

    push

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,13 @@
-# Manifest to fix Nginx open file limit configuration
-exec { 'nginx fix':
-  onlyif   => 'test -e /etc/default/nginx',
-  command  => "sed -i 's/-n 15/-n 4096/' /etc/default/nginx && service nginx restart",
-  provider => 'shell',
+# Manifest to fix Nginx open file limit configuration
+exec { 'nginx fix open file limit':
+  command     => 'sed -i "s/-n 15/-n 4096/" /etc/default/nginx && systemctl restart nginx',
+  onlyif      => 'grep -q "-n 15" /etc/default/nginx',  # Check if the old limit exists
+  path        => ['/usr/bin', '/bin'],  # Ensure the command can find necessary binaries
+  refreshonly => true,  # Execute only when notified
+}
+
+# Notify the exec to run if the configuration file is modified
+file { '/etc/default/nginx':
+  ensure  => file,
+  notify  => Exec['nginx fix open file limit'],
 }
